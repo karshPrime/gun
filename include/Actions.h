ba58a8f2a12195f62a1d7b8f32f9d9ce05712eab
karsh <utkarsh@cyberfear.com>
2025-03-17 14:35:14 +1100

    feat: actions

---
+++ include/Actions.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include "Config.h"
+
+void action_bun( char *[], char *[] );
+void action_run( char *[] );
+void action_build( char *[] );
+void action_clean( void );
+void action_debug( void );
+void action_test( void );
+void action_config( bool );
+
---
+++ src/Actions.c
@@ -0,0 +1,38 @@
+
+#include "Actions.h"
+
+void action_bun( char *aArgs[], char *aFlags[] )
+{
+    //
+}
+
+void action_run( char *aArgs[] )
+{
+    //
+}
+
+void action_build( char *aFlags[] )
+{
+    //
+}
+
+void action_clean( void )
+{
+    //
+}
+
+void action_debug( void )
+{
+    //
+}
+
+void action_test( void )
+{
+    //
+}
+
+void action_config( bool aLocal )
+{
+    //
+}
+
