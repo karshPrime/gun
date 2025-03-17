dd5ea0236b2c07cd92300a89b3ae5ddd857f14b5
karsh <utkarsh@cyberfear.com>
2025-03-17 14:36:50 +1100

    feat: main structure
    ~ updated main structure to include actions and commands

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 
 // main.c
 
+#include "Actions.h"
+#include "Commands.h"
 #include "Config.h"
 #include "Debug.h"
 #include "Parse.h"
@@ -18,13 +20,79 @@ int main( int argc, char *argv[] )
     }
 
     const char *COMMAND = argv[1];
+    Configs *lConfigs = configs_parse();
 
-    for ( uint i = 0; i < argc; i++ )
+
+    if ( parse_check_value( COMMAND, "bun", "x" ) )
     {
-        if      ( parse_check_value( argv[i], "help"   , "h" ) ) { print_help();     }
-        else if ( parse_check_value( argv[i], "version", "v" ) ) { print_version();  }
+        char *lFlags[1]; // size 1 is temporal
+        char *lArgs[1];
+
+        action_bun( lArgs, lFlags );
     }
 
+    else if ( parse_check_value( COMMAND, "run", "r" ) )
+    {
+        char *lArgs[1];
+
+        action_run( lArgs );
+    }
+
+    else if ( parse_check_value( COMMAND, "build", "b" ) )
+    {
+        char *lFlags[1];
+
+        action_run( lFlags );
+    }
+
+    else if ( parse_check_value( COMMAND, "debug",  "d" ) ) { action_debug(); }
+    else if ( parse_check_value( COMMAND, "test",   "t" ) ) { action_test();  }
+    else if ( parse_check_value( COMMAND, "clean",  "c" ) ) { action_clean(); }
+
+    else if ( parse_check_value( COMMAND, "init", "i" ) )
+    {
+        InitArgs lInitArgs = {
+            .Here = false,
+            .NoGit = false,
+            .TemplateIgnore = false
+        };
+
+        // parse remaining flags
+
+        cmd_init( lConfigs, lInitArgs );
+    }
+
+    else if ( parse_check_value( COMMAND, "license", "l" ) )
+    {
+        RecordsArgs lRecords;
+        char *lName;
+
+        cmd_license( lConfigs, lRecords, lName );
+    }
+
+    else if ( parse_check_value( COMMAND, "tempalte", "tm" ) )
+    {
+        RecordsArgs lRecords;
+        Move *lMove;
+
+        cmd_template( lConfigs, lRecords, lMove );
+    }
+
+    else if ( parse_check_value( COMMAND, "config", "z" ) )
+    {
+        bool lIsLocal = false;
+
+        action_config( lIsLocal );
+    }
+
+    else if ( parse_check_value( COMMAND, "help",    "h" ) ) { print_help();    }
+    else if ( parse_check_value( COMMAND, "version", "v" ) ) { print_version(); }
+
+    else
+    {
+        log_error( "Invalid Argument." );
+        print_usage();
+    }
 
     return 0;
 }
