14e0ecca06f92e77b1ab7e37cfa25b825aa51ba2
karsh <utkarsh@cyberfear.com>
2025-03-28 12:09:46 +1100

    feat: actions logic
    + added code to run actions command

--- include/Actions.h
+++ include/Actions.h
@@ -10,11 +10,11 @@
 
 //- Declarations -----------------------------------------------------------------------------------
 
-void action_bun( bool, char *[], char *[] );
-void action_run( bool, char *[] );
-void action_test( bool );
-void action_clean( bool );
-void action_debug( bool );
-void action_config( bool );
-void action_compile( bool, char *[] );
+void action_bun( const bool, char *[], char *[] );
+void action_run( const bool, char *[] );
+void action_test( const bool );
+void action_clean( const bool );
+void action_debug( const bool );
+void action_config( const bool );
+void action_compile( const bool, char *[] );
 
--- src/Actions.c
+++ src/Actions.c
@@ -7,80 +7,129 @@
 #include "Actions.h"
 #include "Debug.h"
 
+#include <stdlib.h>
+#include <unistd.h>
+
 //- Helper Functions -------------------------------------------------------------------------------
 
+void _run_cmd( const bool aRootCD, const char *aCommand )
+{
+    if ( aRootCD )
+    {
+        if ( !system( "git rev-parse --is-inside-work-tree > /dev/null 2>&1" ) )
+        {
+            FILE *lTermOutput = popen( "git rev-parse --show-toplevel", "r" );
+            if ( lTermOutput == NULL )
+            {
+                log_error( "popen" );
+                return;
+            }
+
+            char lTermBuffer[256];
+            if ( fgets( lTermBuffer, 256, lTermOutput ) )
+            {
+                lTermBuffer[strcspn( lTermBuffer, "\n" )] = 0;
+                if ( chdir( lTermBuffer ) )
+                {
+                    log_error( "chdir" );
+                    return;
+                }
+            }
+
+            pclose( lTermOutput );
+        }
+    }
+
+    if ( aCommand )
+    {
+        debug( "%s", aCommand );
+        system( aCommand );
+    }
+    else
+    {
+        log_error( "Command not set in config" );
+    }
+}
 
 
 //- External Calls ---------------------------------------------------------------------------------
 
-void action_bun( bool aLocalConfig, char *aArgs[], char *aFlags[] )
+void action_bun( const bool aLocalConfig, char *aArgs[], char *aFlags[] )
 {
     debug( "action_bun() called" );
 
     ConfigDev *lConfigs = config_parse_dev( aLocalConfig );
 
-    //
+    _run_cmd( lConfigs->RootCD, lConfigs->Build );
+    _run_cmd( false, lConfigs->Run ); // dont cd again; bloat steps
 
     config_free_dev( lConfigs );
 }
 
-void action_run( bool aLocalConfig, char *aArgs[] )
+void action_run( const bool aLocalConfig, char *aArgs[] )
 {
     debug( "action_run() called" );
 
     ConfigDev *lConfigs = config_parse_dev( aLocalConfig );
 
-    //
+    _run_cmd( lConfigs->RootCD, lConfigs->Run );
 
     config_free_dev( lConfigs );
 }
 
-void action_compile( bool aLocalConfig, char *aFlags[] )
+void action_compile( const bool aLocalConfig, char *aFlags[] )
 {
     debug( "action_compile() called" );
 
     ConfigDev *lConfigs = config_parse_dev( aLocalConfig );
 
-    //
+    _run_cmd( lConfigs->RootCD, lConfigs->Build );
 
     config_free_dev( lConfigs );
 }
 
-void action_clean( bool aLocalConfig )
+void action_clean( const bool aLocalConfig )
 {
     debug( "action_clean() called" );
 
     ConfigDev *lConfigs = config_parse_dev( aLocalConfig );
 
-    //
+    _run_cmd( lConfigs->RootCD, lConfigs->Clean );
 
     config_free_dev( lConfigs );
 }
 
-void action_debug( bool aLocalConfig )
+void action_debug( const bool aLocalConfig )
 {
     debug( "action_debug() called" );
 
     ConfigDev *lConfigs = config_parse_dev( aLocalConfig );
 
-    //
+    _run_cmd( lConfigs->RootCD, lConfigs->Debug );
 
     config_free_dev( lConfigs );
 }
 
-void action_test( bool aLocalConfig )
+void action_test( const bool aLocalConfig )
 {
     debug( "action_test() called" );
 
     ConfigDev *lConfigs = config_parse_dev( aLocalConfig );
 
-    //
+    _run_cmd( lConfigs->RootCD, lConfigs->Test );
 
     config_free_dev( lConfigs );
 }
 
-void action_config( bool aLocal )
+void action_config( const bool aLocal )
 {
     debug( "action_config() called" );
+
+    char lCommand[256];
+
+    const char *lConfig = aLocal ? "./" : CONFIG_DIR;
+    sprintf( lCommand, "$EDITOR %s/config.toml", lConfig );
+
+    system( lCommand );
 }
 
