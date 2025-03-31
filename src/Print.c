91f8cf736e219b2614cdea4bbc6e68c6a0c1332c
karsh <utkarsh@cyberfear.com>
2025-03-31 14:49:48 +1100

    style: _private_methods();
    ~ updated print_description with convention for private methods

--- src/Print.c
+++ src/Print.c
@@ -9,13 +9,13 @@
 
 //- Helper Functions -------------------------------------------------------------------------------
 
-void print_description( const char *aFlag, const char *aDescription )
+void _print_description( const char *aFlag, const char *aDescription )
 {
     printf( "%s    --%-22s %s%s%s\n",
             TERM_GREEN, aFlag, TERM_GRAY, aDescription, TERM_RESET );
 }
 
-void print_command( const char *aCmd, const char *aFlags, const char *aDescription )
+void _print_command( const char *aCmd, const char *aFlags, const char *aDescription )
 {
     printf( "%s\n\nUSAGE:\n   %s$ %sdev %s%s %s%s%s\n",
             aDescription, TERM_BLUE, TERM_RED, TERM_YELLOW, aCmd, TERM_BLUE, aFlags, TERM_RESET );
@@ -39,87 +39,87 @@ void print_help( Commands aSpecific )
             break;
 
         case RUN:
-            print_command( "run", "{Args}", "Run programme with optional args" );
+            _print_command( "run", "{Args}", "Run programme with optional args" );
             printf( "\nFLAGS:\n" );
-            print_description( "global", "Override local config with global settings" );
+            _print_description( "global", "Override local config with global settings" );
             break;
 
         case COMPILE:
-            print_command( "compile", "{Flags}", "Compile programme with optional flags" );
+            _print_command( "compile", "{Flags}", "Compile programme with optional flags" );
             printf( "\nFLAGS:\n" );
-            print_description( "global", "Override local config with global settings" );
+            _print_description( "global", "Override local config with global settings" );
             break;
 
         case BUN:
-            print_command( "bun", "", "Build and run programme" );
+            _print_command( "bun", "", "Build and run programme" );
             printf( "\nFLAGS:\n" );
-            print_description( "flags [Flags]", "Specify build flags" );
-            print_description( "args [Args]", "Specify run args" );
-            print_description( "global", "Override local config with global settings" );
+            _print_description( "flags [Flags]", "Specify build flags" );
+            _print_description( "args [Args]", "Specify run args" );
+            _print_description( "global", "Override local config with global settings" );
             break;
 
         case INIT:
-            print_command( "init", "[ProjectName] [Language]", "Initialise a new project." );
+            _print_command( "init", "[ProjectName] [Language]", "Initialise a new project." );
             printf( "\nFLAGS:\n" );
-            print_description( "here", "Create project in current directory" );
-            print_description( "licence [Title]", "Specify licence to use" );
-            print_description( "no-git", "Omit Git initialisation" );
-            print_description( "git-ignore [Args]", "Add to Git ignore list" );
-            print_description( "git-ignore-only [Args]",
+            _print_description( "here", "Create project in current directory" );
+            _print_description( "licence [Title]", "Specify licence to use" );
+            _print_description( "no-git", "Omit Git initialisation" );
+            _print_description( "git-ignore [Args]", "Add to Git ignore list" );
+            _print_description( "git-ignore-only [Args]",
                     "Create ignore list with only the specified args" );
-            print_description( "template [Templates]", "Add template code to project" );
-            print_description( "ignore-template", "Omit template code" );
+            _print_description( "template [Templates]", "Add template code to project" );
+            _print_description( "ignore-template", "Omit template code" );
             break;
 
         case TEMPLATE:
-            print_command( "template", "{Args}", "Manage template files for projects." );
+            _print_command( "template", "{Args}", "Manage template files for projects." );
             printf( "\nFLAGS:\n" );
-            print_description( "list", "List available templates" );
-            print_description( "add [Templates]", "Add template code to project" );
-            print_description( "new [Title]", "Create new template file" );
-            print_description( "manage", "Manage the record of all saved templates" );
-            print_description( "print-dir", "Show template directory" );
+            _print_description( "list", "List available templates" );
+            _print_description( "add [Templates]", "Add template code to project" );
+            _print_description( "new [Title]", "Create new template file" );
+            _print_description( "manage", "Manage the record of all saved templates" );
+            _print_description( "print-dir", "Show template directory" );
             break;
 
         case CLEAN:
-            print_command( "clean", "", "Clean build files" );
+            _print_command( "clean", "", "Clean build files" );
             printf( "\nFLAGS:\n" );
-            print_description( "global", "Override local config with global settings" );
+            _print_description( "global", "Override local config with global settings" );
             break;
 
         case TEST:
-            print_command( "test", "", "Run test files" );
+            _print_command( "test", "", "Run test files" );
             printf( "\nFLAGS:\n" );
-            print_description( "global", "Override local config with global settings" );
+            _print_description( "global", "Override local config with global settings" );
             break;
 
         case DEBUG:
-            print_command( "debug", "", "Run project debugger" );
+            _print_command( "debug", "", "Run project debugger" );
             printf( "\nFLAGS:\n" );
-            print_description( "global", "Override local config with global settings" );
+            _print_description( "global", "Override local config with global settings" );
             break;
 
         case LICENSE:
-            print_command( "licence", "{Args}", "Manage licences for projects." );
+            _print_command( "licence", "{Args}", "Manage licences for projects." );
             printf( "\nFLAGS:\n" );
-            print_description( "list", "List available licences" );
-            print_description( "replace [Title]", "Replace current licence" );
-            print_description( "new [Title]", "Create new licence file" );
-            print_description( "print-dir", "Show licence directory" );
+            _print_description( "list", "List available licences" );
+            _print_description( "replace [Title]", "Replace current licence" );
+            _print_description( "new [Title]", "Create new licence file" );
+            _print_description( "print-dir", "Show licence directory" );
             break;
 
         case CONFIG:
-            print_command( "config", "", "Manage configuration files for the dev utility." );
+            _print_command( "config", "", "Manage configuration files for the dev utility." );
             printf( "\nFLAGS:\n" );
-            print_description( "local", "Edit or create local config file" );
+            _print_description( "local", "Edit or create local config file" );
             break;
 
         case HELP:
-            print_command( "help", "", "Display help menu" );
+            _print_command( "help", "", "Display help menu" );
             break;
 
         case VERSION:
-            print_command( "version", "", "Display utility version" );
+            _print_command( "version", "", "Display utility version" );
             break;
     }
 }
